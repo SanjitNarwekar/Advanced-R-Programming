0459c31a2b99868558b9a9875b6250df9d881f96
Neil@0801 <sanjitn@gmail.com>
2018-03-25 19:06:50 +0530

    Added description

---
+++ fn_factorial_loop.R
@@ -0,0 +1,50 @@
+# Author: Sanjt Narwekar
+# Repo: https://github.com/SanjitNarwekar/Mastering-Software-Development-in-R
+
+
+# function factorial_loop -----------------------------------------------------
+#
+# This function uses a for loop to calculate factorials. The function expects 
+# only a postive integer as an input.
+#
+#------------------------------------------------------------------------------
+
+# load libraries --------------------------------------------------------------
+
+library(purrr)
+library(microbenchmark)
+library(installr)
+
+
+# Define functions ------------------------------------------------------------
+
+factorial_loop <- function(x) {
+  
+  if (x == 0 ||  x == -0 || x == 1){
+    return(1)
+  }
+  
+  if (!check.integer(x)){
+    message("Input ",x," is not an integer. abs(x) used. Results will not tally")
+    
+  }
+  
+  if ( x < 0 ){
+    message("Input ",x," is not positive. abs(x) used. Results will not tally")
+  }
+  
+  for (i in (x - 1):1) {
+    x <- abs(x)
+    x <- x * i
+  }
+  x
+}
+
+# Test functions --------------------------------------------------------------
+
+input <- c(4.5, 1.5)
+
+# Check if all functions produce the same results. R's built-in function
+# factorial() is used to compare the results
+factorial(input)
+map_dbl(input, factorial_loop)
